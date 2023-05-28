67680baacc785d3e64fc2ad950272bfc70fe7825
JenniferEinstein <jennifereinstein@gmail.com>
2023-05-28 15:09:01 -0400

    ReadMe mostly done as well.

---
+++ src/components/About.js
@@ -0,0 +1,40 @@
+import ".About.css"
+
+<>
+<p>draft</p>
+
+[Sarai Thomas](https://github.com/Sarai-ii)
+With a background in business and 
+Software Engineer
+Digital Designer
+Social Media Maven
+🦋 I Paint, Sing, Dance and do anything creative on occasion  
+🦋 Bachelor's Degree in Social Media Marketing, Business & Product Development/Management with minors in financing and entrepreneurship 🦋 
+https://www.codewars.com/users/Sarai-ii
+https://www.linkedin.com/in/saraibusiness/
+My name is Sarai Thomas, I graduated in 2021 at the top of my business class concentrated in; entrepreneurship, e-commerce, business and brand development, social media marketing, and business financing where each of my studies specialized in ethics and professional.
+Post undergrad, My work ethic is very hands-on as a servant leader and motivator. 
+In my free time, I enjoy freelance work that optimizes my marketing skills, from developing brands to identifying trends around the world. As CEO and creative director of a small business called, GLD RZÉ, a new luxury jewelry line, I handle the day-to-day logistics, social media account management, consumer engagement, content creation, and analytics. Standardized company filing procedure by compiling and relabelling three years of user data, improving efficiency across all departments”
+
+
+[Jennifer Einstein](https://github.com/JenniferEinstein)
+Hailing from the Golden State, Jennifer moved to New York City over 20 years ago and never looked back. She aims to make the modern world a better place, one line of code at a time. 
+
+
+[Finesse Poole](https://github.com/fintheehuman)
+A self-starting, diligent hard worker, seeking a position that enables me to expand my already prevalent leadership skills and multi-tasking abilities, while providing exceptional customer service and enhancing the company name.
+Proven team leader
+Life long New Yorker?
+Bach of SCIENCE!
+
+
+[Joanavel Pascual](https://github.com/JoanavelPascual7)
+
+My name is Joanavel Pascual, a determined and experienced professional with a passion for full-stack web development.
+
+spiring Web Developer with a Passion for Social Impact
+
+Coming from a background in social services, I found web development to be both challenging and fascinating. I am based in New York City and look forward to exploring the world of web development further.
+
+
+</>
