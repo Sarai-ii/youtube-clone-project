25c3d7c09953cdc0f14fdf1979808ea7fb5f2852
JenniferEinstein <jennifereinstein@gmail.com>
2023-06-02 22:32:10 -0400

    Working draft of the Navbar with associated CSS component. Also made changes to about.

--- src/components/About.js
+++ src/components/About.js
@@ -4,28 +4,22 @@ import React from 'react'
 function About() {
   return (
     <>
-<p>draft</p>
+<p>About page</p>
 <span className="developer">Sarai Thomas</span> 
-[Sarai Thomas](https://github.com/Sarai-ii)
-Sarai Thomas is a software engineer and digital designer, and also lead on this project. She has combined her background in business with her creativity skills to become the CEO and creative director of GLD RZÉ, a new luxery jewelry line. 🦋
-
+(https://github.com/Sarai-ii)
+<p>Sarai Thomas is a software engineer and digital designer; she is also the lead on this project. Sarai has combined her background in business with her creativity skills to become the CEO and creative director of GLD RZÉ, a new luxery jewelry line.</p>
+<br/>
 <span className="developer">Jennifer Einstein</span> 
-[Jennifer Einstein](https://github.com/JenniferEinstein)
-Hailing from the Golden State, Jennifer moved to New York City over 20 years ago and never looked back. She aims to make the modern world a better place, one line of code at a time. 
-
+(https://github.com/JenniferEinstein)
+<p>Hailing from the Golden State, Jennifer Einstein moved to New York City over 20 years ago and never looked back. She aims to make the modern world a better place, one line of code at a time. </p>
+<br/>
+<span className="developer">Finesse Poole</span>
+(https://github.com/fintheehuman)
+<p>With a background in science, Finesse uses his experience in customer service and leadership to make the web ....</p>
+<br/>
 <span className="developer">Joanavel Pascual</span>
-[Finesse Poole](https://github.com/fintheehuman)
-A self-starting, diligent hard worker, seeking a position that enables me to expand my already prevalent leadership skills and multi-tasking abilities, while providing exceptional customer service and enhancing the company name.
-Proven team leader
-Life long New Yorker?
-Bach of SCIENCE!
-
-<span className="developer">Joanavel Pascual</span>
-[Joanavel Pascual](https://github.com/JoanavelPascual7)
-Joanavel Pascual is a web developer with a passion for social impact. With a background in social work, he is based in New York City. 
-
-
-
+(https://github.com/JoanavelPascual7)
+<p>Joanavel Pascual is a web developer with a passion for social impact. With a background in social work, he is based in New York City. </p>
 </>
   )
 }
