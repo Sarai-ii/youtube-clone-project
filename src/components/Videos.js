d482045335786c1ac911e33c6626e0e304481d7b
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-06-02 22:00:01 -0400

    Video Player Is Showing but not working

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,17 +1,39 @@
 import React from 'react'
+import YouTube from 'react-youtube'
 
 const Videos = ({videos}) => {
-  const videoDetails = videos.snippet 
-  const id = videos.id
+
+ 
+    // const opts = {
+    //   height: '390',
+    //   width: '640',
+    //   playerVars: {
+    //     // https://developers.google.com/youtube/player_parameters
+    //     autoplay: 1,
+    //   },
+    // };
+
+    
+  
+
+  // const onReady = (event => {
+  //   // access to player in all event handlers via event.target
+  //   console.log(event)
+  //   event.target.getPlayerMode();
+  //   // event.target.pauseVideo();
+  // })
 
   return (
     <div>
-      <h1>Videos</h1>
+      <h1 className=''>Videos</h1>
         {
           videos.map(video => {
-            console.log(video.snippet.title)
+            const id = video.id
+            console.log(id)
+
             return (
               <>
+              {/* <YouTube video={video.id} opts={opts} onReady={onReady} />; */}
               <h2>${video.snippet.title}</h2>
               </>
             )
