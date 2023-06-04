6668456f17479edd7c5b2aa02505f01e9d9ac247
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-06-04 11:33:07 -0400

    Readded env

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,29 @@
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
+import YouTubeLogo from "../assets/YouTube-Logo.png";
 
 export default function Footer() {
-    return (
-        <p className="footer"> A YouTube Clone; created by Joanavel, Jennifer, Sarai, and Finesse. </p>
-    )
+  return (
+    <div className="footer">
+      <div className="footer-content">
+        <div className="footer-left">
+          <img src={YouTubeLogo} alt="YouTube logo" className="youtube-logo" />
+          <p className="our-names">
+           <div className="footer-first-text"> Created by:{" "}
+            <a href="https://github.com/JoanavelPascual7">Joanavel Pascual</a>,{" "}
+            <a href="https://github.com/JenniferEinstein">Jennifer Einstein</a>,{" "}
+            <a href="https://github.com/Sarai-ii">Sarai Thomas</a>, and{" "}
+            <a href="https://github.com/fintheehuman">Finesse Poole</a>.
+            </div>
+          </p>
+        </div>
+        <div className="footer-right">
+          <p className="footer-text">
+            YouTube Clone Project &copy; Pursuit 9.6
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
