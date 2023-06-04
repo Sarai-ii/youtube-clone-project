d9c521714b8bfd8d569c5c4017cda84a68fff393
JenniferEinstein <jennifereinstein@gmail.com>
2023-06-04 11:36:07 -0400

    Deleted Nav.css after copying all of that code into Header/NavBar.css. Then I made sure to change which CSS the NavBar was linking to.

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 // import { Navbar, Nav } from "react-bootstrap";
-import "../Nav.css"
+import "./NavBar.css"
 
 export default function NavBar() {
     return (
