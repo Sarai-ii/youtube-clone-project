260420a759985dc142048ecb95a578f487a263e2
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-06-06 19:27:04 -0400

    removed dead links

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -12,11 +12,42 @@ const SideBar = ({ searchHistory }) => {
     <div className={`sidebar ${isOpen ? "open" : ""}`}>
       <button className="sidebar-toggle" onClick={toggleSidebar}></button>
       <div className="sidebar-content">
+
+        <ul className="sidebar-links">
+          <li>
+            <a >Library</a>
+          </li>
+          <div></div>
+          <li>
+            <a >History</a>
+          </li>
+          <div></div>
+          <li>
+            <a >Your Videos</a>
+          </li>
+          <div></div>
+          <li>
+            <a >Watch Later</a>
+          </li>
+          <div></div>
+          <li>
+            <a >Show More</a>
+          </li>
+          <div></div>
+        </ul>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div className="sidebar-divider"></div>
+        <div></div>
         <h2>Search History:</h2>
         <ul>
           {searchHistory.map((search, index) => (
             <li key={index}>{search}</li>
           ))}
+        </ul>
+    
+        <ul>
         </ul>
       </div>
     </div>
