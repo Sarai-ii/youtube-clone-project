84c9922a3c80bb6f256990f73632fcfcec78518b
Joanavel Pascual <joanavelpascual@pursuit.org>
2023-06-05 22:44:13 -0400

    fixed all the errors

--- src/App.js
+++ src/App.js
@@ -6,30 +6,23 @@ import Videos from "./components/Videos";
 import Video from "./components/Video";
 import NavBar from "./components/Header/NavBar";
 import Footer from "./components/Footer";
-import ModalWindow from "./components/ModalWindow";
-import SideBar from "./components/SideBar";
-import SearchBar from "./components/Header/SearchBar";
+
+
 import "./App.css";
 
 function App() {
-  const [searchHistory, setSearchHistory] = useState([]);
 
-  const addSearchToHistory = (search) => {
-    setSearchHistory((prevHistory) => [...prevHistory, search]);
-  };
 
   return (
     <div className="App">
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={<Home addSearchToHistory={addSearchToHistory} />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/videos" element={<Videos />} />
           <Route path="/video/:id" element={<Video />} />
         </Routes>
-        <SideBar className="sidebar" searchHistory={searchHistory} />{" "}
-        {/* Pass searchHistory as prop */}
         <Footer />
       </Router>
     </div>
