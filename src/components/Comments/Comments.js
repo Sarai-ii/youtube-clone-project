50a64b759a63fb8acc9eb7e797e2ebe7e473cb13
Starr <Starr@M-C02F21YMMD6R.nordstrom.net>
2023-06-06 20:46:10 -0400

    Got the form to work with floating but the conditional is no longer working for empty comments

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,17 +5,12 @@ export default function Comments() {
   const { id } = useParams();
 
   const [comments, setComments] = useState([]);
-  // const [name, setName] = useState('');
-  // const [comment, setComment] = useState('');
   const [comment, setComment] = useState({
     name: '',
     comment: ''
-  })
+  });
   const [commentCount, setCommentCount] = useState(0);
   const [editComment, setEditComment] = useState(-1);
-  /*keeping track of the index/position of each comment
-  setting the index to -1 will make it so no comment is automically being edited
-  */
 
   const handleTextChange = (event) => {
     setComment({ ...comment, [event.target.id]: event.target.value });
@@ -23,70 +18,234 @@ export default function Comments() {
 
   const handleComment = (event) => {
     event.preventDefault();
-    if (comments.length){
-      if (editComment !== -1){
-      let updatedComments = [...comments]
-      updatedComments[editComment] = comment
-      setComment(updatedComments);
-      setEditComment(-1)
-      }else{
-      let updatedComments = [...comments, comment] 
+    if (comments.length) {
+      if (editComment !== -1) {
+        let updatedComments = [...comments];
+        updatedComments[editComment] = comment;
+        setComments(updatedComments);
+        setEditComment(-1);
+      } else {
+        let updatedComments = [...comments, comment];
         setComments(updatedComments);
       }
-      setCommentCount(comments.length)
-      setComment({name: '', comment: ''})
+      setCommentCount(comments.length + 1);
+      setComment({ name: '', comment: '' });
+    } else {
+      setComments([comment]);
+      setCommentCount(1);
+      setComment({ name: '', comment: '' });
     }
-  }
+  };
+
   const handleDelete = (index) => {
     let updatedComments = [...comments];
     updatedComments.splice(index, 1);
     setComments(updatedComments);
     setCommentCount(updatedComments.length);
-    if (editComment === index) {setEditComment(-1)}
+    if (editComment === index) {
+      setEditComment(-1);
+    }
   };
-  const handleEdit = (index => {
-    setEditComment(index)
-    setComment(comments[index])
-  })
+
+  const handleEdit = (index) => {
+    setEditComment(index);
+    setComment(comments[index]);
+  };
+
   return (
     <div className=''>
-      <div className='row'>
+      <div className=''>
         <div>
           Total comments: {commentCount}
           <h4>Add a comment....</h4>
-          <hr/>
+          <hr />
         </div>
-
-        <form className="" onSubmit={handleComment}>
-          <label htmlFor='name'>Name: </label>
-          <input type='text' id='name' value={comment.name} onChange={handleTextChange} />
-          
-          <label htmlFor='comment'>Comment: </label>
-          <input type='text' id='comment' value={comment.comment} onChange={handleTextChange} />
-          
-          <input className="row justify-content-right comment-button btn btn-outline-secondary-active" 
-          type='submit' value={editComment !== -1 ? 'Update Comment' : 'Comment'} />
+        <form className='' onSubmit={handleComment}>
+          <div className='form-floating mb-3'>
+            <input
+              type='text'
+              id='name'
+              className='form-control'
+              value={comment.name}
+              onChange={handleTextChange}
+            />
+            <label htmlFor='name'>Name:</label>
+          </div>
+          <div className='form-floating mb-3'>
+            <input
+              type='text'
+              id='comment'
+              className='form-control'
+              value={comment.comment}
+              onChange={handleTextChange}
+            />
+            <label htmlFor='comment'>Comment:</label>
+          </div>
+          <input
+            type='submit'
+            className='row comment-button btn btn-outline-secondary-active mb-3'
+            value={editComment !== -1 ? 'Update Comment' : 'Comment'}
+          />
         </form>
         <hr />
         <ul>
-          {comments.map((com, index) => {
+          {
+          comments.map((com, index) => {
             return (
               <li key={index}>
-                {com.name}:  {com.comment}
-                <div className="btn">
-                <button 
-                className="btn btn-outline-secondary p-1"
-                onClick={() => handleDelete(index)}>Delete</button>
-                <button 
-                className="btn btn-outline-secondary p-1"
-                onClick={() => handleEdit(index)}>Edit</button>
+                {com.name}: {com.comment}
+                <div className='btn'>
+                  <button
+                    className='btn btn-outline-secondary p-1'
+                    onClick={() => handleDelete(index)}
+                  >
+                    Delete
+                  </button>
+                  <button
+                    className='btn btn-outline-secondary p-1'
+                    onClick={() => handleEdit(index)}
+                  >
+                    Edit
+                  </button>
                 </div>
-              
               </li>
             );
           })}
         </ul>
+        <br />
+        <section>
+          <h3>Recommended Videos</h3>
+          <br />
+          <br />
+          <br />
+        </section>
       </div>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from 'react';
+// import { useParams } from 'react-router-dom';
+
+// export default function Comments() {
+//   const { id } = useParams();
+
+//   const [comments, setComments] = useState([]);
+//   // const [name, setName] = useState('');
+//   // const [comment, setComment] = useState('');
+//   const [comment, setComment] = useState({
+//     name: '',
+//     comment: ''
+//   })
+//   const [commentCount, setCommentCount] = useState(0);
+//   const [editComment, setEditComment] = useState(-1);
+//   /*keeping track of the index/position of each comment
+//   setting the index to -1 will make it so no comment is automically being edited
+//   */
+
+//   const handleTextChange = (event) => {
+//     setComment({ ...comment, [event.target.id]: event.target.value });
+//   };
+
+//   const handleComment = (event) => {
+//     event.preventDefault();
+//     if (comments.length){
+//       if (editComment !== -1){
+//       let updatedComments = [...comments]
+//       updatedComments[editComment] = comment
+//       setComment(updatedComments);
+//       setEditComment(-1)
+//       }else{
+//       let updatedComments = [...comments, comment] 
+//         setComments(updatedComments);
+//       }
+//       setCommentCount(comments.length)
+//       setComment({name: '', comment: ''})
+//     }
+//   }
+//   const handleDelete = (index) => {
+//     let updatedComments = [...comments];
+//     updatedComments.splice(index, 1);
+//     setComments(updatedComments);
+//     setCommentCount(updatedComments.length);
+//     if (editComment === index) {setEditComment(-1)}
+//   };
+//   const handleEdit = (index => {
+//     setEditComment(index)
+//     setComment(comments[index])
+//   })
+//   return (
+//     <div className=''>
+//       <div className=''>
+       
+//         <div>
+//           Total comments: {commentCount}
+//           <h4>Add a comment....</h4>
+//           <hr/>
+//         </div>
+//         <form className="" onSubmit={handleComment}>
+//           <label htmlFor='name'>Name: </label>
+//           <input type='text' id='name' 
+//           className="w-100"
+//           value={comment.name} onChange={handleTextChange} />
+          
+//           <label htmlFor='comment float'>Comment: </label>
+//           <input type='text' id='comment' 
+//           className="w-100"
+//           value={comment.comment} onChange={handleTextChange} />
+          
+//           <input type='submit' 
+//           className="row comment-button btn btn-outline-secondary-active"
+//           value={editComment !== -1 ? 'Update Comment' : 'Comment'} />
+//         </form>
+//         <hr />
+//         <ul>
+//           {comments.map((com, index) => {
+//             return (
+//               <li key={index}>
+//                 {com.name}:  {com.comment}
+//                 <div className="btn">
+//                 <button 
+//                 className="btn btn-outline-secondary p-1"
+//                 onClick={() => handleDelete(index)}>Delete</button>
+//                 <button 
+//                 className="btn btn-outline-secondary p-1"
+//                 onClick={() => handleEdit(index)}>Edit</button>
+//                 </div>
+              
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </div>
+//     </div>
+//   );
+// }
--- src/components/Video.js
+++ src/components/Video.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, {useState} from 'react';
+import { useParams, Link} from 'react-router-dom';
 import YouTube from 'react-youtube';
 
 // <------Components------>
@@ -10,26 +10,48 @@ export default function Video() {
 
   const onReady = (event) => {
     // access to player in all event handlers via event.target
-    console.log(videos);
     event.target.playVideo();
   };
 
   const opts = {
-    height: '500',
-    width: '640',
+    height: '720',
+    width: '1280',
     playerVars: {
       // https: `developers.google.com/youtube/player_parameters`,
       autoplay: 1,
     },
   };
 
+  const [showMore, setShowMore] = useState(false);
+
+  const handleToggle = () => {
+    setShowMore(!showMore);
+  };
 
 
   return (
-    <div className="container">
-      <div>
-        <YouTube videoId={id} onReady={onReady} opts={opts} />
-        <h2>{videos?.snippet?.title}</h2>
+    <div className="container ">
+      <div className='row m-2 justify-content-center'>
+        <div className="col-lg-12 col-md-10 col-sm-8">
+          <YouTube videoId={id} onReady={onReady} opts={opts} />
+          {/* <h2 className="text-center mt-3">{videos.snippet.title}</h2> 
+          // Can't add video title to the component without rerouting the useEffect to app.js or into a fetch file. 
+          // I don't want to do that on this branch(maybe once the team is complete.) */}
+        </div>
+        <div className="Description box bg-light rounded p-3">
+          <p>
+            <strong>235,000 views June 5,2023</strong>
+          </p>
+
+          {showMore && (
+            <p>This page was coded and styled by Sarai Thomas</p>
+          )}
+
+          <Link onClick={handleToggle} 
+            className=" text-decoration-none text-secondary">
+            {showMore ? 'Show Less' : 'Show More'}
+          </Link>
+        </div>
         <Comments />
       </div>
     </div>
